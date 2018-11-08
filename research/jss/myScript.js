6a3e5c2e6401045050fe26757ee8c0b57814e907
Xie <10425755+suruixie@users.noreply.github.com>
2018-11-08 15:14:18 -0500

    Update myScript.js

--- research/jss/myScript.js
+++ research/jss/myScript.js
@@ -1,13 +1,14 @@
 function updateSlider(slideAmount) {
-//get the element
-var display = document.getElementById("chosen");
-//show the amount
-display.innerHTML=slideAmount;
-//get the element
-var pic = document.getElementById("pic");
-//set the dimensions
-pic.style.marginLeft=calc((slideAmount+"px") * 0.1639);
-//pic.style.marginLeft=(slideAmount*0.1639)+"px";
-//pic.style.height=slideAmount+"px";
+  document.getElementById('output').innerHTML = slideAmount;
+  //get the element
+  var display = document.getElementById('slider').value=0;
+  //show the amount
+  display.innerHTML=slideAmount;
+  //get the element
+  var pic = document.getElementById("pic");
+  //set the dimensions
+  pic.style.marginLeft=calc((slideAmount+"px") * 0.1639);
+  //pic.style.marginLeft=(slideAmount*0.1639)+"px";
+  //pic.style.height=slideAmount+"px";
 
 }
