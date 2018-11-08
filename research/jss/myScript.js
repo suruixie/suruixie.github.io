65b5f2ab0814d047f589544b8cc8372c86a42b8b
Xie <10425755+suruixie@users.noreply.github.com>
2018-11-08 10:34:48 -0500

    Update myScript.js

--- research/jss/myScript.js
+++ research/jss/myScript.js
@@ -3,3 +3,12 @@ function sliderChange(slideAmount) {
 }
 document.getElementById('slider').value = 0;
 
+//get the element
+var display = document.getElementById("chosen");
+//show the amount
+display.innerHTML=slideAmount;
+
+//get the element
+var pic = document.getElementById("pic");
+//set the dimensions
+pic.style.margin-left=slideAmount;
