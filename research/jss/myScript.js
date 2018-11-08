ac45e8ae6d88e4a2137bf18e8c9ebb6f6b2461d5
Xie <10425755+suruixie@users.noreply.github.com>
2018-11-08 10:38:55 -0500

    Update myScript.js

--- research/jss/myScript.js
+++ research/jss/myScript.js
@@ -4,11 +4,6 @@ function sliderChange(slideAmount) {
 document.getElementById('slider').value = 0;
 
 //get the element
-var display = document.getElementById("chosen");
-//show the amount
-display.innerHTML=slideAmount;
-
-//get the element
 var pic = document.getElementById("pic");
 //set the dimensions
 pic.style.height=slideAmount+"%";
