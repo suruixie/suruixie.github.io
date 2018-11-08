f7cd4f20240eabd6b5b35bac0fc3a4e4296d066f
Xie <10425755+suruixie@users.noreply.github.com>
2018-11-08 11:39:07 -0500

    Update myScript.js

--- research/jss/myScript.js
+++ research/jss/myScript.js
@@ -1,4 +1,4 @@
 function sliderChange(slideAmount) {
     document.getElementById('output').innerHTML = slideAmount;
 }
-document.getElementById('slider').value = 0;
+document.getElementById('slide').value = 0;
