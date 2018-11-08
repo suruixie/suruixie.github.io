0e27bae7f577e8acc7842a66729a8231e5e0a238
Xie <10425755+suruixie@users.noreply.github.com>
2018-11-08 13:33:01 -0500

    Update myScript.js

--- research/jss/myScript.js
+++ research/jss/myScript.js
@@ -6,7 +6,7 @@ display.innerHTML=slideAmount;
 //get the element
 var pic = document.getElementById("pic");
 //set the dimensions
-pic.style.width=slideAmount+"%";
-pic.style.height=slideAmount+"%";
+pic.style.width=slideAmount+"px";
+pic.style.height=slideAmount+"px";
 
 }
