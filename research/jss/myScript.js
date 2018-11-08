675c45cd2422285ea563737998e01ece6ad26082
Xie <10425755+suruixie@users.noreply.github.com>
2018-11-08 16:54:46 -0500

    Update myScript.js

--- research/jss/myScript.js
+++ research/jss/myScript.js
@@ -1,5 +1,10 @@
 function updateSlider(slideAmount) {
   document.getElementById('output').innerHTML = slideAmount;
+  //get the element
+  var display = document.getElementById("chosen");
+  //show the amount
+  display.innerHTML=slideAmount;
+  //get the element
   var pic = document.getElementById("pic");
   //set the dimensions
   pic.style.marginLeft=calc((slideAmount+"px") * 0.1639);
