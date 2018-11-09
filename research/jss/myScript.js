428d2a02847f026f9e4443cb9227fbde90d3dfa6
Xie <10425755+suruixie@users.noreply.github.com>
2018-11-09 08:07:04 -0500

    Update myScript.js

--- research/jss/myScript.js
+++ research/jss/myScript.js
@@ -9,10 +9,10 @@ function updateSlider(slideAmount) {
   var picL = document.getElementById("picL");
   //set the dimensions
   var tmp = slideAmount;
-  var tmp1 = tmp * 0.2732;
+  var tmp1 = tmp * 0.21858;
   pic.style.marginLeft=tmp1+"px";
   picL.style.marginLeft=tmp1+"px";
-  //calc( ( slideAmount + "px") * 0.2732 );
+  //calc( ( slideAmount + "px") * 0.21858 );
   //pic.style.marginLeft=slideAmount+"px";
   //pic.style.height=slideAmount+"px";
 
